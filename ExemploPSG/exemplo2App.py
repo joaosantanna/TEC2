46e5f28e1cead64cca89097394f5042aa12c9c38
Joao Santanna <joaosantanna@yahoo.com.br>
2024-01-25 20:01:25 -0300

    projeto exemplo2App.py refatorado

--- ExemploPSG/exemplo2App.py
+++ ExemploPSG/exemplo2App.py
@@ -5,36 +5,40 @@ autor : joao
 
 import PySimpleGUI as sg
 from equacao2g import equacao2g
+import sys
 
-desenho =[
+sg.theme('BlueMono')
+desenho = [
     [sg.Text('Calculo para equacao do 2 grau')],
     [sg.Text('A:'), sg.Input(key='a')],
     [sg.Text('B:'), sg.Input(key='b')],
     [sg.Text('C:'), sg.Input(key='c')],
-    [sg.Text('X1 =', key='x1') ],
+    [sg.Text('X1 =', key='x1')],
     [sg.Text('X2 =', key='x2')],
     [sg.Button('Calcular'), sg.Button('Sair'), sg.Button('Limpar')]
 ]
 
-janela = sg.Window('Equacao 2 grau', font=('Courier New',12),size=(400,250))
+janela = sg.Window('Equacao 2 grau', font=('Helvetica', 12), size=(400, 250))
 janela.Layout(desenho)
 while True:
-    botao, valores = janela.Read()
-    if botao=='Calcular':
-        a =float(valores['a'])
+    evento, valores = janela.Read() # evento pega os botoes pressionados e outros eventos
+    # que podem ocorrer no programa
+
+    if evento == 'Calcular':
+        a = float(valores['a'])
         b = float(valores['b'])
         c = float(valores['c'])
         r = equacao2g(a, b, c)
-        if r != False:
+        if r is not False:
             janela['x1'].update(f'X1 = {r[0]:.4f}')
             janela['x2'].update(f'X2 = {r[1]:.4f}')
         else:
             janela['x1'].update('Nao exitem raizes reais')
             janela['x2'].update('')
 
-    if botao=='Sair':
+    if evento == 'Sair' or evento == sg.WIN_CLOSED:
         break
-    if botao == 'Limpar':
+    if evento == 'Limpar':
         janela['a'].update('')
         janela['b'].update('')
         janela['c'].update('')
@@ -42,3 +46,4 @@ while True:
         janela['x2'].update('')
 
 janela.close()
+sys.exit(0)
